bec5d8d43c35608e8dc34e345945006a877c037d
bruno-venturini <brunofreloventurini@outlook.com>
2025-05-29 21:10:44 -0300

    #5 Adicionando comentários na exceção principal do sistema

--- src/main/java/com/handbook/handbookapi/exceptions/GameRuleException.java
+++ src/main/java/com/handbook/handbookapi/exceptions/GameRuleException.java
@@ -1,5 +1,10 @@
 package com.handbook.handbookapi.exceptions;
 
+/**
+ * Exception thrown when a game rule is violated.
+ * This exception is used to indicate that a specific game rule has been broken,
+ * and it can be used to provide feedback to the user or log the violation.
+ */
 public class GameRuleException extends RuntimeException {
 
     public GameRuleException(String message) {
